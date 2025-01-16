2671ac8ddf66d976f3001f4303b11214f57a7a52
Eshaan Sullerey <eshaansullerey@gmail.com>
2025-01-15 19:14:58 -0800

    Changes according to the design changes:
    1. on-off control for two intakes
    2. on-off control for flywheel
    3. goal control for flywheel
    4. drivetrain control (part done)

--- number3/main.py
+++ number3/main.py
@@ -7,19 +7,26 @@ from vex import *
 import urandom
 
 
-TOP_GOAL_VELOCITY = 100
-TOP_GOAL_TORQUE = 100
-BOTTOM_GOAL_VELOCITY = 20
-BOTTOM_GOAL_TORQUE = 20
+INIT_TG_FLYWHEEL_VELOCITY = 100
+INIT_BG_FLYWHEEL_VELOCITY = 60
+FLYWHEEL_TORQUE = 100
+INTAKE_VELOCITY = 100
+INTAKE_TORQUE = 100
+DRIVETRAIN_VELOCITY = 100
+DRIVETRAIN_TORQUE = 100
+
+flywheel_tg_velocity = INIT_TG_FLYWHEEL_VELOCITY
+flywheel_bg_velocity = INIT_BG_FLYWHEEL_VELOCITY
 
 # Port configurations
 # All L(left) R(right) directions are defined from viewpoint
 # looking from behind the bot towards the flyweel
 FLYWHEEL_L_PORT = Ports.PORT6
-FLYWHEEL_R_PORT = Ports.PORT12
-INTAKE_PORT = Ports.PORT5
-DRIVETRAIN_R_PORT = Ports.PORT7
-DRIVETRAIN_L_PORT = Ports.PORT1
+FLYWHEEL_R_PORT = Ports.PORT12 
+DRIVETRAIN_L_PORT = Ports.PORT5
+DRIVETRAIN_R_PORT = Ports.PORT11
+BOTTOM_INTAKE_PORT = Ports.PORT4
+TOP_INTAKE_PORT = Ports.PORT10
 
 # Brain should be defined by default
 brain=Brain()
@@ -27,10 +34,9 @@ brain.screen.print("N3-MAIN\n")
 brain.screen.next_row()
 brain.screen.print("FW_R_L:12,6")
 brain.screen.next_row()
-brain.screen.print("IT:5")
+brain.screen.print("DT_R_L:11,5")
 brain.screen.next_row()
-brain.screen.print("DT_R_L:7,1")
-
+brain.screen.print("IT_T_B;10,4")
 
 wait(10, MSEC)
 
@@ -40,9 +46,8 @@ controller = Controller()
 flywheel_motor_R = Motor(FLYWHEEL_R_PORT, True)
 flywheel_motor_L = Motor(FLYWHEEL_L_PORT, False)
 flywheel = MotorGroup(flywheel_motor_R, flywheel_motor_L)
-
-intake = Motor(INTAKE_PORT, True)
-
+top_intake = Motor(TOP_INTAKE_PORT, False)
+bottom_intake = Motor(BOTTOM_INTAKE_PORT, True)
 Drivetrain_motor_L = Motor(DRIVETRAIN_L_PORT, True)
 Drivetrain_motor_R = Motor(DRIVETRAIN_R_PORT, True)
 Drivetrain = MotorGroup(Drivetrain_motor_R, Drivetrain_motor_L)
@@ -66,19 +71,28 @@ myVariable = 0
 launcher_speed = 0
 in_top_goal_mode = True
 is_flywheel_on = False
-is_intake_on = False
+is_top_intake_on = False 
+is_bottom_intake_on = False
 
-def when_started1():
-    global myVariable, launcher_speed, in_top_goal_mode, is_flywheel_on, is_intake_on
-    flywheel.set_max_torque(TOP_GOAL_TORQUE, PERCENT)
-    flywheel.set_velocity(TOP_GOAL_VELOCITY, PERCENT)
+
+def when_started():
+    global launcher_speed, in_top_goal_mode, is_flywheel_on, flywheel_tg_velocity, flywheel_bg_velocity
+    global top_intake, bottom_intake, is_top_intake_on, is_bottom_intake_on
+    flywheel_tg_velocity = INIT_TG_FLYWHEEL_VELOCITY
+    flywheel_bg_velocity = INIT_BG_FLYWHEEL_VELOCITY
+    flywheel.set_max_torque(FLYWHEEL_TORQUE, PERCENT)
+    flywheel.set_velocity(flywheel_tg_velocity, PERCENT)
+    top_intake.set_max_torque(INTAKE_TORQUE, PERCENT)
+    top_intake.set_velocity(INTAKE_VELOCITY, PERCENT)
+    bottom_intake.set_max_torque(INTAKE_TORQUE, PERCENT)
+    bottom_intake.set_velocity(INTAKE_VELOCITY, PERCENT)   
     in_top_goal_mode = True
     is_flywheel_on = False
-    is_intake_on = False
-
+    is_top_intake_on = False
+    is_bottom_intake_on = False
 
 def flywheel_on_off():
-    global myVariable, launcher_speed, is_flywheel_on
+    global flywheel, is_flywheel_on
     if is_flywheel_on:
         flywheel.stop()
         is_flywheel_on = False
@@ -87,39 +101,80 @@ def flywheel_on_off():
         is_flywheel_on = True
 
 def flywheel_goal_select():
-    global myVariable, launcher_speed, in_top_goal_mode
+    global launcher_speed, in_top_goal_mode
     if in_top_goal_mode:
-        flywheel.set_max_torque(BOTTOM_GOAL_TORQUE, PERCENT)
-        flywheel.set_velocity(BOTTOM_GOAL_VELOCITY, PERCENT)
+        flywheel.set_max_torque(FLYWHEEL_TORQUE, PERCENT)
+        flywheel.set_velocity(flywheel_tg_velocity, PERCENT)
         in_top_goal_mode = False
     else:
-        flywheel.set_max_torque(TOP_GOAL_TORQUE, PERCENT)
-        flywheel.set_velocity(TOP_GOAL_VELOCITY, PERCENT)
+        flywheel.set_max_torque(FLYWHEEL_TORQUE, PERCENT)
+        flywheel.set_velocity(flywheel_bg_velocity, PERCENT)
         in_top_goal_mode = True
 
-def onevent_controllerbuttonLUp_pressed_0():
-    global myVariable, launcher_speed
-    flywheel.spin(FORWARD)
-
-def intake_on_off():
-    global intake, is_intake_on
-    if is_intake_on:
-        intake.stop()
-        is_intake_on = False 
+def top_intake_on_off():
+    global top_intake, is_top_intake_on
+    if is_top_intake_on:
+        top_intake.stop()
+        is_top_intake_on = False
     else:
-        intake.spin(FORWARD)
-        is_intake_on = True
+        top_intake.spin(FORWARD)
+        is_top_intake_on = True
 
+def bottom_intake_on_off():
+    global bottom_intake, is_bottom_intake_on
+    if is_bottom_intake_on:
+        bottom_intake.stop()
+        is_bottom_intake_on = False
+    else:
+        bottom_intake.spin(FORWARD)
+        is_bottom_intake_on = True
 
-def onevent_controllerbuttonLDown_pressed_0():
-    pass
+def incr_flywheel_tg_velocity():
+    global flywheel_tg_velocity
+
+    updated_tg_velocity = flywheel_tg_velocity + 10
+    if updated_tg_velocity > 100:
+        updated_tg_velocity = 100
+    
+    flywheel_tg_velocity = updated_tg_velocity
+
+def decr_flywheel_tg_velocity():
+    global flywheel_tg_velocity
+
+    updated_tg_velocity = flywheel_tg_velocity - 10
+    if updated_tg_velocity < 0:
+        updated_tg_velocity = 0
+
+    flywheel_tg_velocity = updated_tg_velocity
+
+def incr_flywheel_bg_velocity():
+    global flywheel_bg_velocity
+
+    updated_bg_velocity = flywheel_bg_velocity + 10
+    if updated_bg_velocity > 100:
+        updated_bg_velocity = 100
+
+        flywheel_bg_velocity = updated_bg_velocity
+
+def decr_flywheel_bg_velocity():
+    global flywheel_bg_velocity
+    
+    updated_bg_velocity = flywheel_bg_velocity - 10
+    if updated_bg_velocity < 0:
+        updated_bg_velocity = 0
+
+        flywheel_bg_velocity = updated_bg_velocity
 
 # system event handlers
-controller.buttonLUp.pressed(intake_on_off)
-controller.buttonLDown.pressed(onevent_controllerbuttonLDown_pressed_0)
-controller.buttonRUp.pressed(flywheel_on_off)
-controller.buttonRDown.pressed(flywheel_goal_select)
+controller.buttonLUp.pressed(flywheel_on_off) 
+controller.buttonLDown.pressed(flywheel_goal_select)
+controller.buttonRUp.pressed(bottom_intake_on_off)
+controller.buttonRDown.pressed(top_intake_on_off)
+controller.buttonEUp.pressed(incr_flywheel_tg_velocity)
+controller.buttonEDown.pressed(decr_flywheel_tg_velocity)
+controller.buttonFUp.pressed(incr_flywheel_bg_velocity)
+controller.buttonFDown.pressed(decr_flywheel_bg_velocity)
 # add 15ms delay to make sure events are registered correctly.
 wait(15, MSEC)
 
-when_started1()
+when_started()
