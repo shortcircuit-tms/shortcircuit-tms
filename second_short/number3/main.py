babdeb78dfcb30a2fedd5fe2c2f817899b855667
Eshaan Sullerey <eshaansullerey@gmail.com>
2025-01-31 16:34:44 -0800

    Updated code for second shot.

--- second_short/number3/main.py
+++ second_short/number3/main.py
@@ -7,37 +7,31 @@ from vex import *
 import urandom
 
 
-INIT_TG_FLYWHEEL_VELOCITY = 100
-INIT_BG_FLYWHEEL_VELOCITY = 34
-FLYWHEEL_TORQUE = 100
 INTAKE_VELOCITY = 100
 INTAKE_TORQUE = 100
 DRIVETRAIN_VELOCITY = 100
 DRIVETRAIN_TORQUE = 100
-column_number = 1
-
-flywheel_tg_velocity = INIT_TG_FLYWHEEL_VELOCITY
-flywheel_bg_velocity = INIT_BG_FLYWHEEL_VELOCITY
+CATAPULT_VELOCITY = 100
+CATAPULT_TORQUE = 100
+CONVEYOR_VELOCITY = 100
+CONVEYOR_TORQUE = 100
 
 # Port configurations
 # All L(left) R(right) directions are defined from viewpoint
 # looking from behind the bot towards the flyweel
-FLYWHEEL_L_PORT = Ports.PORT3
-FLYWHEEL_R_PORT = Ports.PORT12 
-DRIVETRAIN_L_PORT = Ports.PORT5
-DRIVETRAIN_R_PORT = Ports.PORT11
-BOTTOM_INTAKE_PORT = Ports.PORT4
-TOP_INTAKE_PORT = Ports.PORT10
+ 
+CONVEYER_L_PORT = Ports.PORT1
+CATAPULT_PORT = Ports.PORT2
+INTAKE_PORT = Ports.PORT4
+CONVEYER_R_PORT = Ports.PORT5
+DRIVETRAIN_R_PORT = Ports.PORT9
+DRIVETRAIN_L_PORT = Ports.PORT10
+DISTANCE_SENSOR_PORT = Ports.PORT11
 
 # Brain should be defined by default
 brain=Brain()
-brain.screen.print("N3-MAIN\n")
+brain.screen.print("SH_N3\n")
 brain.screen.next_row()
-brain.screen.print("FW_R_L:12,6")
-brain.screen.next_row()
-brain.screen.print("DT_R_L:11,5")
-brain.screen.next_row()
-brain.screen.print("IT_T_B;10,4")
 brain.screen.new_line()
 
 wait(10, MSEC)
@@ -45,11 +39,11 @@ wait(10, MSEC)
 # Robot configuration code
 brain_inertial = Inertial()
 controller = Controller()
-flywheel_motor_R = Motor(FLYWHEEL_R_PORT, True)
-flywheel_motor_L = Motor(FLYWHEEL_L_PORT, False)
-flywheel = MotorGroup(flywheel_motor_R, flywheel_motor_L)
-top_intake = Motor(TOP_INTAKE_PORT, False)
-bottom_intake = Motor(BOTTOM_INTAKE_PORT, True)
+conveyor_motor_r = Motor(CONVEYER_R_PORT, True)
+conveyor_motor_l = Motor(CONVEYER_L_PORT, False)
+conveyor = MotorGroup(conveyor_motor_r, conveyor_motor_l)
+intake_motor = Motor(INTAKE_PORT, False)
+catapult_motor = Motor(CATAPULT_PORT, False)
 
 # set up drivetrain
 left_drive_smart = Motor(DRIVETRAIN_L_PORT, 1.0, False)
@@ -74,118 +68,56 @@ initializeRandomSeed()
 
 myVariable = 0
 launcher_speed = 0
-in_top_goal_mode = True
-is_flywheel_on = False
-is_top_intake_on = False 
-is_bottom_intake_on = False
+is_conveyer_on = False
+is_intake_on = False
+is_catapult_on = False
 
 
 def when_started():
-    global launcher_speed, in_top_goal_mode, is_flywheel_on, flywheel_tg_velocity, flywheel_bg_velocity, column_number
-    global top_intake, bottom_intake, is_top_intake_on, is_bottom_intake_on
-    flywheel_tg_velocity = INIT_TG_FLYWHEEL_VELOCITY
-    flywheel_bg_velocity = INIT_BG_FLYWHEEL_VELOCITY
-    flywheel.set_max_torque(FLYWHEEL_TORQUE, PERCENT)
-    flywheel.set_velocity(flywheel_tg_velocity, PERCENT)
-    top_intake.set_max_torque(INTAKE_TORQUE, PERCENT)
-    top_intake.set_velocity(INTAKE_VELOCITY, PERCENT)
-    bottom_intake.set_max_torque(INTAKE_TORQUE, PERCENT)
-    bottom_intake.set_velocity(INTAKE_VELOCITY, PERCENT)   
-    in_top_goal_mode = True
-    is_flywheel_on = False
-    is_top_intake_on = False
-    is_bottom_intake_on = False
+    global is_conveyer_on, conveyor_motor_r, conveyor_motor_l
+    global is_intake_on, intake_motor
+    global is_catapult_on, catapult_motor
 
-def flywheel_on_off():
-    global flywheel, is_flywheel_on
-    if is_flywheel_on:
-        flywheel.stop()
-        is_flywheel_on = False
+    is_conveyer_on = False
+    conveyor.set_max_torque(CONVEYOR_TORQUE, PERCENT)
+    conveyor.set_velocity(CONVEYOR_VELOCITY, PERCENT)
+    is_intake_on = False
+    intake_motor.set_max_torque(INTAKE_TORQUE, PERCENT)
+    intake_motor.set_velocity(INTAKE_VELOCITY, PERCENT)
+    is_catapult_on = False
+    catapult_motor.set_max_torque(CATAPULT_TORQUE, PERCENT)
+    catapult_motor.set_velocity(CATAPULT_VELOCITY, PERCENT)
+
+def conveyer_on_off():
+    global is_conveyer_on, conveyor
+    if is_conveyer_on:
+        conveyor.stop()
+        is_conveyer_on = False
     else:
-        flywheel.spin(FORWARD)
-        is_flywheel_on = True
+        conveyor.spin(FORWARD)
+        is_conveyer_on = True
 
-def flywheel_goal_select():
-    global launcher_speed, in_top_goal_mode, flywheel_bg_velocity, flywheel_tg_velocity
-    if in_top_goal_mode:
-        flywheel.set_max_torque(FLYWHEEL_TORQUE, PERCENT)
-        flywheel.set_velocity(flywheel_tg_velocity, PERCENT)
-        in_top_goal_mode = False
+def catapult_on_off():
+    global is_catapult_on, catapult_motor
+    if is_catapult_on:
+        catapult_motor.stop()
+        is_catapult_on = False
     else:
-        flywheel.set_max_torque(FLYWHEEL_TORQUE, PERCENT)
-        flywheel.set_velocity(flywheel_bg_velocity, PERCENT)
-        in_top_goal_mode = True
+        catapult_motor.spin(FORWARD)
+        is_catapult_on = True
 
-def top_intake_on_off():
-    global top_intake, is_top_intake_on
-    if is_top_intake_on:
-        top_intake.stop()
-        is_top_intake_on = False
+def intake_on_off():
+    global is_intake_on, intake_motor
+    if is_intake_on:
+        intake_motor.stop()
+        is_intake_on = False
     else:
-        top_intake.spin(FORWARD)
-        is_top_intake_on = True
-
-def bottom_intake_on_off():
-    global bottom_intake, is_bottom_intake_on
-    if is_bottom_intake_on:
-        bottom_intake.stop()
-        is_bottom_intake_on = False
-    else:
-        bottom_intake.spin(FORWARD)
-        is_bottom_intake_on = True
-
-def incr_flywheel_tg_velocity():
-    global flywheel_tg_velocity
-
-    updated_tg_velocity = flywheel_tg_velocity + 2
-    if updated_tg_velocity > 100:
-        updated_tg_velocity = 100
-    
-    flywheel_tg_velocity = updated_tg_velocity
-    print_flywheel_veocity()    
+        intake_motor.spin(FORWARD)
+        is_intake_on = True
 
 
-def decr_flywheel_tg_velocity():
-    global flywheel_tg_velocity
-
-    updated_tg_velocity = flywheel_tg_velocity - 2
-    if updated_tg_velocity < 0:
-        updated_tg_velocity = 0
-
-    flywheel_tg_velocity = updated_tg_velocity
-    print_flywheel_veocity()    
-
-def incr_flywheel_bg_velocity():
-    global flywheel_bg_velocity
-
-    updated_bg_velocity = flywheel_bg_velocity + 2
-    if updated_bg_velocity > 100:
-        updated_bg_velocity = 100
-
-    flywheel_bg_velocity = updated_bg_velocity
-    print_flywheel_veocity()    
-
-def decr_flywheel_bg_velocity():
-    global flywheel_bg_velocity
-    
-    updated_bg_velocity = flywheel_bg_velocity - 2
-    if updated_bg_velocity < 0:
-        updated_bg_velocity = 0
-
-    flywheel_bg_velocity = updated_bg_velocity
-    print_flywheel_veocity()    
-
-def print_row(text, row_number, delete_row_before_print=True):
-    if delete_row_before_print:
-        brain.screen.clear_row(row_number)
-    brain.screen.set_cursor(row_number, 1)
-    brain.screen.print(text)
-                       
-def print_flywheel_veocity():
-    global flywheel_tg_velocity, flywheel_bg_velocity
-    text = "FWV_T_B=" + str(flywheel_tg_velocity) + "," + str(flywheel_bg_velocity)
-    print_row(text=text, row_number=5)
-
+## Strat of drivetrain code that was derived from VEX IQ
+## Do not touch this code unless you know what you are doing
 vexcode_initial_drivetrain_calibration_completed = False
 def calibrate_drivetrain():
     # Calibrate the Drivetrain Inertial
@@ -258,15 +190,12 @@ def rc_auto_loop_function_controller():
 # Calibrate the Drivetrain
 calibrate_drivetrain()
 
+## End of the drivetrain code
+
 # system event handlers
-controller.buttonLUp.pressed(flywheel_on_off) 
-controller.buttonLDown.pressed(flywheel_goal_select)
-controller.buttonRUp.pressed(bottom_intake_on_off)
-controller.buttonRDown.pressed(top_intake_on_off)
-controller.buttonFUp.pressed(incr_flywheel_bg_velocity)
-controller.buttonFDown.pressed(decr_flywheel_bg_velocity)
-controller.buttonEUp.pressed(incr_flywheel_tg_velocity)
-controller.buttonEDown.pressed(decr_flywheel_tg_velocity)
+controller.buttonLUp.pressed(catapult_on_off) 
+controller.buttonRUp.pressed(conveyer_on_off)
+controller.buttonRDown.pressed(intake_on_off)
 # add 15ms delay to make sure events are registered correctly.
 wait(15, MSEC)
 
